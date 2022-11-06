99bfbebd8505478853f2d373313ff9d1927c2ce1
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-11-06 21:38:20 +0530

    Source code added

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,1 +1,15 @@
 
+import os as o
+import shutil as s
+
+path=input('Enter Your Path:')
+list_dir=o.listdir(path)
+print(list_dir)
+for i in list_dir:
+	name,ext=o.path.splitext(i)
+	ext=ext[1:]
+	if o.path.exists(path+"/"+ext):
+		s.move(path+"/"+i,path+"/"+ext+"/"+i)
+	else:
+		o.makedirs(path+"/"+ext)
+		s.move(path+"/"+i,path+"/"+ext+"/"+i)
