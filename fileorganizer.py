a547438bc718d7723399d5ceb6aea8b53677939a
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-11-07 19:18:38 +0530

    comments added

--- fileorganizer.py
+++ fileorganizer.py
@@ -2,17 +2,17 @@ import os  #provides functions for interacting with the operating system.
 import shutil  #helps in process of copying, moving and removal of files and directories.
 import easygui as e  # provides functions for gui creation.
 
-title="FILE ORGANIZER"
+title="FILE ORGANIZER" # variable to store title of the GUI
 e_msg="Enter Path"
 exit="EXIT"
-heading="Error"
+heading="Error" # varible to show error message.
 path=e.enterbox(e_msg,title)   # user input for path of the folder.
 if path=="":
 	e.msgbox("Please! Try Again!",heading,exit)
 else:		
     ls=os.listdir(path)
     for i in ls:
-	name,ext=os.path.splitext(i)
+	name,ext=os.path.splitext(i)  #path string spliting into to two variables.
 	ext=ext[1:]                   #for example .pdf----->pdf
 	if ext=="":                   #skipping a folder.
 	    continue
