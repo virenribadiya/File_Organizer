ec49d11b279d07379ac97a5ac53b4173b388520b
Viren Ribadiya <vribadiya278@rku.ac.in>
2022-11-07 18:51:17 +0530

    optimized with gui
    GUI is created using easygui module in python.

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,16 +1,26 @@
-// The OS module in Python provides functions for interacting with the operating system. OS comes under Python's standard utility modules.
-import os as o 
-// The shutil module offers a number of high-level operations on files and collections of files.
-import shutil as s
+import os  
+import shutil  
+import easygui as e 
 
-path=input('Enter Your Path:')
-list_dir=o.listdir(path)
-print(list_dir)
-for i in list_dir:
-	name,ext=o.path.splitext(i)
-	ext=ext[1:]
-	if o.path.exists(path+"/"+ext):
-		s.move(path+"/"+i,path+"/"+ext+"/"+i)
+title="FILE ORGANIZER"
+e_msg="Enter Path"
+exit="EXIT"
+heading="Error"
+path=e.enterbox(e_msg,title)   # user input for path of the folder.
+if path=="":
+	e.msgbox("Please! Try Again!",heading,exit)
+else:		
+    ls=os.listdir(path)
+    for i in ls:
+	name,ext=os.path.splitext(i)
+	ext=ext[1:] #for example .pdf----->pdf
+	if ext=="": #skipping a folder.
+	    continue
+	elif os.path.exists(path+"/"+ext):
+	    shutil.move(path+"/"+i,path+"/"+ext+"/"+i)
 	else:
-		o.makedirs(path+"/"+ext)
-		s.move(path+"/"+i,path+"/"+ext+"/"+i)
+	    os.makedirs(path+"/"+ext)
+	    shutil.move(path+"/"+i,path+"/"+ext+"/"+i)
+    but="Good Job!"
+    e.msgbox("Excecution Successful!!","",but)
+	
